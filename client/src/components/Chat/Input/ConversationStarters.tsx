318c88ff0d92bc171725fcb97f7a3319d2643af1
nikolaex <nikola.stevanovic@exxeta.com>
2025-12-16 12:14:22 +0100

    feat: Add conversation starters to landing page and update package-lock.json

--- client/src/components/Chat/Input/ConversationStarters.tsx
+++ client/src/components/Chat/Input/ConversationStarters.tsx
@@ -4,12 +4,15 @@ import { useChatContext, useAgentsMapContext, useAssistantsMapContext } from '~/
 import { useGetAssistantDocsQuery, useGetEndpointsQuery } from '~/data-provider';
 import { getIconEndpoint, getEntity } from '~/utils';
 import { useSubmitMessage } from '~/hooks';
+import { ZapIcon } from 'lucide-react';
 
 const ConversationStarters = () => {
   const { conversation } = useChatContext();
   const agentsMap = useAgentsMapContext();
   const assistantMap = useAssistantsMapContext();
   const { data: endpointsConfig } = useGetEndpointsQuery();
+
+  console.log('conversation=, ', conversation);
 
   const endpointType = useMemo(() => {
     let ep = conversation?.endpoint ?? '';
@@ -41,13 +44,15 @@ const ConversationStarters = () => {
     assistant_id: conversation?.assistant_id,
   });
 
-  const conversation_starters = useMemo(() => {
+  // TODO-1: Somehow store Agent's conversation starters in centralized storage used in this project
+  // TODO-2: Write tests which test this functionality (4 most recent conversation starters to be displayed on Landing page)
+  const conversation_starters: string[] = useMemo(() => {
     if (entity?.conversation_starters?.length) {
       return entity.conversation_starters;
     }
 
     if (isAgent) {
-      return [];
+      return entity?.conversation_starters ?? [];
     }
 
     return documentsMap.get(entity?.id ?? '')?.conversation_starters ?? [];
@@ -59,21 +64,37 @@ const ConversationStarters = () => {
     [submitMessage],
   );
 
+  // For testing purposes, adding some hardcoded conversation starters
+  // conversation_starters.push('Welche Vorteile hat der elektrische Lkw im Vergleich zum Diesel?');
+  // conversation_starters.push('Vergleiche unsere Hauptmodelle miteinander.');
+  // conversation_starters.push('Nenne mir die wichtigsten Verkaufsargumente für den neuen eActros.');
+  // conversation_starters.push('Welche Argumente sprechen für den Einsatz von Fleetboard?');
+
+  // conversation_starters.push('Wofür steht Daimler Truck als Unternehmen?');
+  // conversation_starters.push('Welche nachhaltigen Lösungen bietet Daimler Truck an?');  
+
+  console.log('conversation_starters values:');
+  conversation_starters.forEach((elem) => console.log(elem));
+
   if (!conversation_starters.length) {
     return null;
   }
 
   return (
-    <div className="mt-8 flex flex-wrap justify-center gap-3 px-4">
+    <div className="mt-4 flex flex-col justify-center gap-1 px-4">
+      <div className='flex flex-row gap-1 '>
+        <ZapIcon className='text-gray-400 size-4 mt-[2px]'/>
+        <p className='text-gray-400 text-[13px] font-medium pb-2'>Suggestions:</p>
+      </div>
       {conversation_starters
         .slice(0, Constants.MAX_CONVO_STARTERS)
         .map((text: string, index: number) => (
           <button
             key={index}
             onClick={() => sendConversationStarter(text)}
-            className="relative flex w-40 cursor-pointer flex-col gap-2 rounded-2xl border border-border-medium px-3 pb-4 pt-3 text-start align-top text-[15px] shadow-[0_0_2px_0_rgba(0,0,0,0.05),0_4px_6px_0_rgba(0,0,0,0.02)] transition-colors duration-300 ease-in-out fade-in hover:bg-surface-tertiary"
+            className="relative flex flex-col w-200 cursor-pointer gap-1 rounded-2xl  px-3 pb-2 pt-2 text-start align-top text-[15px] shadow-[0_0_2px_0_rgba(0,0,0,0.05),0_4px_6px_0_rgba(0,0,0,0.02)] transition-colors duration-300 ease-in-out fade-in hover:bg-surface-tertiary"
           >
-            <p className="break-word line-clamp-3 overflow-hidden text-balance break-all text-text-secondary">
+            <p className="line-clamp-3 overflow-hidden text-balance text-text-secondary">
               {text}
             </p>
           </button>
